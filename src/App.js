2deb89833eca5f306e483829e2503f2fb3a0bea4
Rohit Kushwah <rohit.kushwah@gemsessence.com>
2023-04-13 12:59:02 +0530

    Added routing in the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DashBoard from "./components/DashBoard";
+import Regiter from "./components/Regiter";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Logout from "./components/Logout";
 
 function App() {
   return (
-
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<DashBoard />} />
+        <Route path="/register" element={<Regiter />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route
+          path="*"
+          element={<h1 style={{ textAlign: "center" }}>404. Not Found</h1>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">DashBoard</Link>
+        </li>
+        <li>
+          <Link to="/register">Regiter</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/logout">Logout</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
